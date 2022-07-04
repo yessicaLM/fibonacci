70de7f1c5a8709a3f68b5fd8815d4c4d96de15ee
yessica <yessica@leanmind.es>
2022-07-04 10:00:19 +0100

    REFACTOR: fibonacci calculator file

--- src/core/fibonacci.ts
+++ src/core/fibonacci.ts
@@ -1,17 +1,19 @@
+const sequenceCalculator = (givenNumber: number): number[] => {
+  let nextNumber = 0, firstNumber = 0, secondNumber = 1;
+  const listOfNumbers = [];
+
+  while (nextNumber < givenNumber) {
+    listOfNumbers.push(nextNumber);
+    firstNumber = secondNumber;
+    secondNumber = nextNumber;
+    nextNumber = firstNumber + secondNumber;
+  }
+  return listOfNumbers;
+}
+
 export const fibonacciCalculator = (number: number): number[] => {
   if (number === 0) return [0];
   if (number === 1) return [0, 1];
 
-  let totalSum = 0;
-  let numbA = 0;
-  let numbB = 1;
-  const listOfNumbers = [];
-
-  while(totalSum < number) {
-    listOfNumbers.push(totalSum);
-    numbA = numbB;
-    numbB = totalSum;
-    totalSum = numbA + numbB;
-  }
-  return listOfNumbers;
+  return sequenceCalculator(number);
 }
