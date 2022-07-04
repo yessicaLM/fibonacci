ea6debd0b758832bf705d19971078cc566c26265
yessica <yessica@leanmind.es>
2022-07-04 10:08:07 +0100

    REFACTOR: test files

--- src/tests/fibonacci.test.ts
+++ src/tests/fibonacci.test.ts
@@ -9,22 +9,37 @@ import { fibonacciCalculator } from '../core/fibonacci';
 
 describe('Fibonacci', () => {
   it('given 0, returns [0]', () => {
-    expect(fibonacciCalculator(0)).toEqual([0]);
+    const givenValue = 0;
+    const expectedValue = [0];
+
+    expect(fibonacciCalculator(givenValue)).toEqual(expectedValue);
   });
 
   it('given 1, returns [0, 1]', () => {
-    expect(fibonacciCalculator(1)).toEqual([0, 1]);
+    const givenValue = 1;
+    const expectedValue = [0, 1];
+
+    expect(fibonacciCalculator(givenValue)).toEqual(expectedValue);
   });
 
   it('given 3, returns [0, 1, 1, 2]', () => {
-    expect(fibonacciCalculator(3)).toEqual([0, 1, 1, 2]);
+    const expectedValue = [0, 1, 1, 2];
+    const givenValue = 3;
+
+    expect(fibonacciCalculator(givenValue)).toEqual(expectedValue);
   });
 
   it('given 5, returns [0,1,1,2,3]', () => {
-    expect(fibonacciCalculator(5)).toEqual([0,1,1,2,3]);
+    const expectedValue = [0, 1, 1, 2, 3];
+    const givenValue = 5;
+    
+    expect(fibonacciCalculator(givenValue)).toEqual(expectedValue);
   });
 
   it('given 10, returns [0,1,1,2,3,5,8]', () => {
-    expect(fibonacciCalculator(10)).toEqual([0,1,1,2,3,5,8]);
+    const expectedValue = [0,1,1,2,3,5,8];
+    const givenValue = 10;
+
+    expect(fibonacciCalculator(givenValue)).toEqual(expectedValue);
   });
 });
