43123eb622e3f54652cba3095426f4950b8c4d54
yessica <yessica@leanmind.es>
2022-07-04 09:46:26 +0100

    GREEN: given 3, returns [0, 1, 1, 2]

--- src/core/fibonacci.ts
+++ src/core/fibonacci.ts
@@ -1,4 +1,17 @@
 export const fibonacciCalculator = (number: number): number[] => {
+  if (number === 0) return [0];
   if (number === 1) return [0, 1];
-  return [0];
+
+  let totalSum = 0;
+  let numbA = 0;
+  let numbB = 1;
+  const listOfNumbers = [];
+
+  while(totalSum < number) {
+    listOfNumbers.push(totalSum);
+    numbA = numbB;
+    numbB = totalSum;
+    totalSum = numbA + numbB;
+  }
+  return listOfNumbers;
 }
